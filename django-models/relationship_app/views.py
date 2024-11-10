9f53090154955dca458c04a82803336a36839b6f
Sabab Asmae <sababasmae@gmail.com>
2024-11-10 22:55:31 +0100

    Edit path

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,10 +5,10 @@ from .models import Book, Library
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view for a library's details
 class LibraryDetailView(View):
     def get(self, request, pk):
         library = Library.objects.get(pk=pk)
-        return render(request, 'library_detail.html', {'library': library})
+        return render(request, 'relationship_app/library_detail.html', {'library': library})
