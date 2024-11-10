bc6591d902ff83c9fe590b00b51f950b47fccf95
Jihane <moutalebjihane90@gmail.com>
2024-11-10 22:16:55 +0000

    Add relationship models and sample queries

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,29 +4,24 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+
 
 # Book Model: A book is written by one author
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
-    def __str__(self):
-        return self.title
+
 
 # Library Model: A library contains many books
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name="libraries")
 
-    def __str__(self):
-        return self.name
+
 
 # Librarian Model: Each library has exactly one librarian
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
 
-    def __str__(self):
-        return self.name
