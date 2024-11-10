6a99e801cf56b87f696ffe3ff136eda79bf62f80
Sabab Asmae <sababasmae@gmail.com>
2024-11-10 23:14:03 +0100

    task 2

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Library
 from .models import Book
 from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+
 
 # Function-based view to list all books
 def list_books(request):
@@ -13,3 +18,30 @@ class LibraryDetailView(DetailView):
     def get(self, request, pk):
         library = Library.objects.get(pk=pk)
         return render(request, 'relationship_app/library_detail.html', {'library': library})
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+# Login view
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('list_books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return render(request, 'logout.html')
