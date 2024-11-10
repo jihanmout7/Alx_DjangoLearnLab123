0691a047f64563ba960a96c41830134442012fcc
Sabab Asmae <sababasmae@gmail.com>
2024-11-10 23:31:41 +0100

    Edit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,14 +1,29 @@
+"""
+URL configuration for LibraryProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', views.list_books, name='list_books'),
+     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+      path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
-    
 ]
