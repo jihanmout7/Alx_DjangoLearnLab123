dcefaa94ff4abca19877a4a50a60ac3593445374
Sabab Asmae <sababasmae@gmail.com>
2024-11-10 23:01:24 +0100

    Edit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import list_books
 
 urlpatterns = [
     path('admin/', admin.site.urls),
