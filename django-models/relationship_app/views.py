be80f2600f78cf28ac952ae6c99c54ceec58c4d4
Sabab Asmae <sababasmae@gmail.com>
2024-11-10 22:58:32 +0100

    Edit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from .models import Library
 from .models import Book
+from django.views.generic.detail import DetailView
 
 # Function-based view to list all books
 def list_books(request):
