d90f5dcd47c68e4349a204177a114ef1b7a23fba
Sabab Asmae <sababasmae@gmail.com>
2024-11-10 22:59:35 +0100

    Edit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.views import View
 from .models import Library
 from .models import Book
 from django.views.generic.detail import DetailView
@@ -10,7 +9,7 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view for a library's details
-class LibraryDetailView(View):
+class LibraryDetailView(DetailView):
     def get(self, request, pk):
         library = Library.objects.get(pk=pk)
         return render(request, 'relationship_app/library_detail.html', {'library': library})
