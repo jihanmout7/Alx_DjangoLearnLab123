b590cdf0984d029c2794b07441b26cd6acd739bc
jihanmout7 <moutalebjihane90@gmail.com>
2024-11-10 20:41:43 +0000

    Delete django-models/LibraryProject directory

--- django-models/LibraryProject/bookshelf/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book  # Import the Book model
-
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Fields to display in the admin list view
-    search_fields = ('title', 'author')  # Enable search functionality for these fields
-    list_filter = ('title', 'author', 'publication_year')
-admin.site.register(Book, BookAdmin)  # Register the Book model with the admin site
