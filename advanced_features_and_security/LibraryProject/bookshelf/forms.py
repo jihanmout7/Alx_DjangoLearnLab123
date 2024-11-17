400a8473538efd731fd27a04c550cef309e3edd9
Jihane <moutalebjihane90@gmail.com>
2024-11-17 15:07:30 +0000

    first commit

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
+# LibraryProject/bookshelf/forms.py
+
+from django import forms
 from .models import Book
-from .forms import SearchForm
 
-def ExempleForm(request):
-    form = SearchForm(request.GET)
-    results = []
-
-    if form.is_valid():  # Only proceed if the form data is valid
-        query = form.cleaned_data['search_term']
-        results = Book.objects.filter(title__icontains=query)
-
-    return render(request, 'search_results.html', {'form': form, 'results': results})
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'genre', 'publication_date', 'description']
+        widgets = {
+            'publication_date': forms.SelectDateWidget(years=range(1900, 2025)),
+        }
