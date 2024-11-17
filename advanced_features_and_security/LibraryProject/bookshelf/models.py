dd48b41ea5ff917f49d5f0cc1920804b4a77d4ce
Jihane <moutalebjihane90@gmail.com>
2024-11-17 13:08:45 +0000

    first commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -17,6 +17,14 @@ class Book(models.Model):
     
     def __str__(self):
         return self.username
+    
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
 
     
 
