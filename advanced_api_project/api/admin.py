a8382d223f87322444d5b620d53d98e53c5a8722
Jihane <moutalebjihane90@gmail.com>
2024-11-29 13:38:00 +0000

    new commit

--- advanced_api_project/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Author, Book
-
-# Register the models to make them visible in the admin interface
-admin.site.register(Author)
-admin.site.register(Book)
